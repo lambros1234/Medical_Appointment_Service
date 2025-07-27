2d822451bb773ffd005eea6608ce3da2d9e2e2c9
lambros1234 <lambroshalatsis154@gmail.com>
2025-07-27 23:17:52 +0300

    CreateDefaultAdmin method in AppConfig. If admin doesn't exist, he is created.

--- src/main/java/com/medibook/appointment/config/AppConfig.java
+++ src/main/java/com/medibook/appointment/config/AppConfig.java
@@ -1,5 +1,9 @@
 package com.medibook.appointment.config;
 
+import com.medibook.appointment.entities.Role;
+import com.medibook.appointment.entities.User;
+import com.medibook.appointment.repositories.RoleRepository;
+import com.medibook.appointment.repositories.UserRepository;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -7,15 +11,49 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Set;
 
 
 @Configuration
 public class AppConfig {
 
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder encoder;
+
+    public AppConfig(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        encoder = new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommandLineRunner createDefaultAdmin() {
+        //Check if admin exists
+        return args -> {
+            if (userRepository.findByUsername("admin1").isEmpty()) {
+                System.out.println("Creating default admin user...");
+
+                Role adminRole = roleRepository.findByName("ROLE_ADMIN")
+                        .orElseGet(() -> roleRepository.save(new Role("ROLE_ADMIN")));
+
+                User admin = new User();
+                admin.setUsername("admin1");
+                admin.setEmail("admin1@example.com");
+                admin.setPassword(encoder.encode("admin123"));
+                admin.setEnabled(true);
+                admin.setRoles(Set.of(adminRole));
+
+                userRepository.save(admin);
+                System.out.println("Admin created.");
+            }
+        };
+    }
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {return new BCryptPasswordEncoder();}
 
